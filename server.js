8bf10c06ebb1aac10afe7fe19b5a4b931fb4c48c
Glitch (kycbot) <none>
2018-08-16 21:00:29 +0000

    🚀🗾 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// 'client.on('message')' commands are triggered when the
+// specified message is read in a text channel that the bot is in.
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+client.on('message', msg => {
+  console.log(msg.author);
+  if (!msg.author.bot && msg.content.match(/rholang/i)) {
+    msg.reply('RHOlang is the language of RChain');
+  }
+});
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+client.login("NDc5NzAyMDcyNDY4NDM5MDUw.Dldnxw.F9yENIDf4iQDzvOT_SF96XBs_v4");
+
+
