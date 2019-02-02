e2625200b09a5d25d45b657cd364b3051ada1eb9
Jim Whitescarver <jimscarver@gmail.com>
2019-02-02 14:55:00 +0000

    vote fix, mailbox recieve added

--- dummyuser.h
+++ dummyuser.h
@@ -1,5 +1,5 @@
 #define $myusername "dummyuser"
 #define $myprivkey "aaaa"
 #define $locker_dummyuser `rho:id:n953kxiotz6bjhe8o5xndzi7mgwo5jxumccbbroe37ue7j3eyosrd4`
-#define $locker_nonce_dummyuser 44
+#define $locker_nonce_dummyuser 46
 #define $inbox_dummyuser `rho:id:ya687kjhwtyj877fedddh9jiac5ydfpcjycyfa16cduhrhmwc7n738`
